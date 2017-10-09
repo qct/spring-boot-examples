f7bae8a5e7ed8cf3fd6bc1ae616a2edd5ff3f526
qct <quchentao@163.com>
2017-10-09 02:33:04 +0000

    spring boot profile

---
+++ spring-boot-profile/src/main/java/alex/MyConfig.java
@@ -0,0 +1,21 @@
+package alex;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>Created by Damon.Q on 2017/10/7.
+ */
+@Configuration
+public class MyConfig {
+
+    @Value("${user.dada}")
+    private String name;
+
+    @Bean
+    public String test() {
+        System.out.println("|||||||||||||||||||||||||||||||" + name);
+        return null;
+    }
+}
---
+++ spring-boot-profile/src/test/java/alex/ApplicationTest.java
@@ -0,0 +1,18 @@
+package alex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p>Created by Damon.Q on 2017/10/6.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void name() throws Exception {
+    }
+}
