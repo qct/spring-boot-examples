140b7947a36336f30ae102b12166a64c6d90bca1
qct <quchentao@163.com>
2018-03-05 04:20:19 +0000

    jooq

--- spring-boot-jooq/src/main/java/alex/JooqApplication.java
+++ spring-boot-jooq/src/main/java/alex/JooqApplication.java
@@ -1,5 +1,12 @@
 package alex;
 
+import static alex.jooq.tables.Customer.CUSTOMER;
+
+import alex.jooq.tables.records.CustomerRecord;
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +18,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  */
 @SpringBootApplication
-public class JooqApplication {
+public class JooqApplication implements CommandLineRunner {
+
+    @Autowired
+    private DSLContext create;
 
     public static void main(String[] args) {
         SpringApplication.run(JooqApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Result<CustomerRecord> records = create.selectFrom(CUSTOMER).where(CUSTOMER.FIRST_NAME.like("%e%")).fetch();
+        records.forEach(System.out::print);
     }
 }
