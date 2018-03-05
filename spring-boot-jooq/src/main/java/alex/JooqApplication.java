30cf862a3b35c806f42d0fd815f7b1c05e65a9d3
qct <quchentao@163.com>
2018-03-05 02:49:35 +0000

    jooq

---
+++ spring-boot-jooq/src/main/java/alex/JooqApplication.java
@@ -0,0 +1,19 @@
+package alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * default description.
+ *
+ * @author qct
+ * @date 2018/3/5
+ * @since 1.0
+ */
+@SpringBootApplication
+public class JooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqApplication.class, args);
+    }
+}
---
+++ spring-boot-jooq/src/test/java/alex/JooqApplicationTest.java
@@ -0,0 +1,22 @@
+package alex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * default description.
+ *
+ * @author qct
+ * @date 2018/3/5
+ * @since 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JooqApplicationTest {
+
+    @Test
+    public void testContextLoads() {
+    }
+}
