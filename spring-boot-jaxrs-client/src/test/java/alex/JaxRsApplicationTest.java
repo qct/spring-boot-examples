bca8589584c8a4eff3a449d6b21d6b292d4e3ddc
qct <tango.qct@gmail.com>
2023-11-05 22:18:23 +0400

    Fix spring-boot-jaxrs-client testcases

--- spring-boot-jaxrs-client/src/test/java/alex/JaxRsApplicationTest.java
+++ spring-boot-jaxrs-client/src/test/java/alex/JaxRsApplicationTest.java
@@ -5,21 +5,34 @@ import javax.ws.rs.client.ClientBuilder;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.proxy.WebResourceFactory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import si.mazi.rescu.RestProxyFactory;
 
 /** Created by qct on 2017/11/6. */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class JaxRsApplicationTest {
 
-    private static final String SERVICE_URI = "http://127.0.0.1:8080";
     public static final String EXPECTED = "world";
+
+    @LocalServerPort private int port;
+
+    private String serviceUri;
+
+    @BeforeEach
+    void setUp() {
+        serviceUri = "http://localhost:" + port;
+    }
 
     /** works unless remove dependency of cxf-rt-rs-client */
     @Test
     public void testMessageWithJerseyProxyClient() throws Exception {
         ClientConfig c = new ClientConfig().register(Endpoint.class);
         Client resource = ClientBuilder.newClient(c);
-        Endpoint proxy = WebResourceFactory.newResource(Endpoint.class, resource.target(SERVICE_URI));
+        Endpoint proxy = WebResourceFactory.newResource(Endpoint.class, resource.target(serviceUri));
 
         Greeting greeting = proxy.message();
         Assertions.assertEquals(EXPECTED, greeting.getMessage());
@@ -37,7 +50,7 @@ public class JaxRsApplicationTest {
 
     @Test
     public void testMessageWithRescu() throws Exception {
-        Endpoint proxy = RestProxyFactory.createProxy(Endpoint.class, SERVICE_URI);
+        Endpoint proxy = RestProxyFactory.createProxy(Endpoint.class, serviceUri);
 
         Greeting greeting = proxy.message();
         Assertions.assertEquals(EXPECTED, greeting.getMessage());
