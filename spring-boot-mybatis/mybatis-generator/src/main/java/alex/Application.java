b76f77be01b767637e95494f5bb2f64ddf6fa853
qct <tango.qct@gmail.com>
2023-11-26 18:26:09 +0400

    Init DDL and DML

--- spring-boot-mybatis/mybatis-generator/src/main/java/alex/Application.java
+++ spring-boot-mybatis/mybatis-generator/src/main/java/alex/Application.java
@@ -1,15 +1,21 @@
 package alex;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** Created by qct on 2017/10/5. */
 @SpringBootApplication
 @MapperScan("alex.dao")
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Thread.sleep(1000 * 60 * 60);
     }
 }
