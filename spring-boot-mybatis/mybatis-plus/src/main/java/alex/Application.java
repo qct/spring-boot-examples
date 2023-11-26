37ae50f950770c8490c54ac9e3c4d703b09c50c0
qct <tango.qct@gmail.com>
2023-11-26 14:38:53 +0400

    group mybatis modules

---
+++ spring-boot-mybatis/mybatis-plus/src/main/java/alex/Application.java
@@ -0,0 +1,15 @@
+package alex;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Created by qct on 2017/10/5. */
+@SpringBootApplication
+@MapperScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
